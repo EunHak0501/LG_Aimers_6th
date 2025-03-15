c6a601c3559a522d654e4e2a67d5723b283a0c49
EunHak0501 <eom0314@naver.com>
2025-03-15 21:03:03 +0900

    Update

--- cal_auc.py
+++ cal_auc.py
@@ -4,11 +4,12 @@ import os
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 
-ROOT_DIR = "./LG_Aimers_6th/"
-Data_DIR = "./data/"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # cal_auc.py가 있는 폴더
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
 
 def calculate_auc(y_pred, seed=1):
-    train_data = pd.read_csv(os.path.join(ROOT_DIR, Data_DIR, "train.csv"))
+    train_data = pd.read_csv(os.path.join(DATA_DIR, "train.csv"))
 
     train, test = train_test_split(train_data, test_size=0.2, random_state=seed, stratify=train_data['임신 성공 여부'])
     y_test = test['임신 성공 여부'].copy()
